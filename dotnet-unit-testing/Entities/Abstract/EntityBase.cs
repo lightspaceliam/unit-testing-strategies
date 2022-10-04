da45f356f0cdce069b2cf98f187f3c6da74d6419
Liam Handasyde <liam.handasyde@health.telstra.com>
2022-10-04 11:48:55 +1100

    Minor comment for Entity Id property.

--- dotnet-unit-testing/Entities/Abstract/EntityBase.cs
+++ dotnet-unit-testing/Entities/Abstract/EntityBase.cs
@@ -6,12 +6,11 @@ namespace Entities.Abstract
     {
         /// <summary>
         /// From what I have seen so far, Fhir's Id's support a mix of Id types: numbers, guids, ... [A-Za-z0-9\-\.]{1,64}
+        /// Will leave generation & further validation out of scope for now.
         /// Ref: https://hl7.org/fhir/datatypes.html#primitive
         /// </summary>
-        // TODO: test regex
         [Required(ErrorMessage = "Id is required.")]
         [StringLength(64, ErrorMessage = "Id has a min {2} and max {1} length.", MinimumLength = 1)]
-        [RegularExpression("/^[a-zA-Z0-9-_]+$/", ErrorMessage = "Only alphanumeric dash and period characters allowed.")]
         public string Id { get; set; } = default!;
     }
 }
